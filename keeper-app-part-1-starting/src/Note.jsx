7ffa7fe590f3c55d29118e3bccc180cae49e30c8
Mohit Bhatt <38008228+Mohitbhatt2710@users.noreply.github.com>
2022-06-21 20:17:55 +0530

    keeper app q:

---
+++ keeper-app-part-1-starting/src/Note.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Note = () => {
+  return (
+    <div className='note'>
+      <h1>Title</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, laboriosam!</p>
+    </div>
+  )
+}
+
+export default Note
